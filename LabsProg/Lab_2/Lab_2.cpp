8a33b096b025399609e9258f83c9c358a6c14dfe
Dante Leapman <DanteLeapman@mail.ru>
2020-07-07 03:34:23 +0300

    Fixed Lab2 2/3

--- LabsProg/Lab_2/Lab_2.cpp
+++ LabsProg/Lab_2/Lab_2.cpp
@@ -12,6 +12,25 @@ public:
         this -> a = a;
     };
 
+    safe_long_long_t &operator++(){
+        this -> a++;
+        return *this;
+    }
+    safe_long_long_t &operator--(){
+        this -> a--;
+        return *this;
+    }
+    safe_long_long_t &operator++(int val){
+        safe_long_long_t temp(*this);
+        this -> a++;
+        return temp;
+    }
+    safe_long_long_t &operator--(int val){
+        safe_long_long_t temp(*this);
+        this -> a--;
+        return temp;
+    }
+
     safe_long_long_t &operator=(const safe_long_long_t &a) {
         this -> a = a.a;
         return *this;
@@ -115,6 +134,13 @@ public:
     ~safe_c_array_t() {
         delete[] elem;
     }
+    safe_c_array_t operator=(const safe_c_array_t &array) {
+        this->size = array.size;
+        for (int i = 0; i < size; i++) {
+            this -> elem[i] = array.elem[i];
+        }
+        return *this;
+    }
 };
 
 
@@ -123,18 +149,65 @@ int main() {
     safe_long_long_t num2(200);
     std::cout << num1 + num2 << std::endl;
     safe_long_long_t num3(0);
-    std::cin >> num3;
+    //std::cin >> num3;
     std::cout << num3 << std::endl;
     std::cout << num1/num2 << std::endl;
+    std::cout << "num1 is " << num1 << std::endl; // num1 = 1010
+    num1++;
+    std::cout << "num1 now is " << num1 << std::endl;
+    num1--;
+    --num1;
+    std::cout << "num1 now is different again " << num1 << std::endl;
+    std::cout << std::endl;
 
+//    safe_c_array_t<int> a(10);
+//    a[9] = 1000;
+//    std::cout << a[9] << " " << a[0] << std::endl;
+//    safe_c_array_t<int> b(a);
+//    safe_c_array_t<int> test(6);
+//    b[9] = 10;
+//    std::cout << a[9] << " " << b[9] << " " << a[0] << std::endl;
+//    for (int i = 0; i < a.size; i++){
+//        std::cout << a[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    for (int i = 0; i < b.size; i++){
+//        std::cout << b[i] << " ";
+//    }
+//    std::cout << std::endl;
+//    for (int i = 0; i < test.size; i++){
+//        std::cout << test[i] << " ";
+//    }
+//    std::cout << std::endl;
 
-    safe_c_array_t<int> a(10);
-    a[9] = 1000;
-    std::cout << a[9] << " " << a[0] << std::endl;
-    safe_c_array_t<int> b(a);
-    b[9] = 10;
-    std::cout << a[9] << " " << b[9] << " " << a[0] << std::endl;
-    for (int i = 0; i < a.size; i++){
-        std::cout << a[i] << " ";
+    safe_c_array_t<int> x(5);
+    for (int i = 0; i < x.size; i++){
+        std::cout << x[i] << " ";
     }
+    std::cout << " - x is here" << std::endl;
+    x[0] = 3;
+    x[2] = 55;
+    for (int i = 0; i < x.size; i++){
+        std::cout << x[i] << " ";
+    }
+    std::cout << " - x is here 2" << std::endl;
+    safe_c_array_t<int> b(7);
+    for (int i = 0; i < b.size; i++){
+        std::cout << b[i] << " ";
+    }
+    std::cout << " - b is here" << std::endl;
+    // Присвоение через operator =;
+    b = x;
+    for (int i = 0; i < x.size; i++){
+        std::cout << x[i] << " ";
+    }
+    std::cout << " - x is here (operator =)" << std::endl;
+    std::cout << x.size << " it's x size" << std::endl;
+    for (int i = 0; i < b.size; i++){
+        std::cout << b[i] << " ";
+    }
+    std::cout << " - b is here (operator =)" << std::endl;
+    std::cout << b.size << " it's b size" << std::endl;
+
+    return 0;
 }
